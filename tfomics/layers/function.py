prettify and fixed softmax

tfomics/layers/function.py
@@ -133,10 +133,7 @@ def activation(z, function='relu', **kwargs):
 		output = tf.nn.sigmoid(z, **kwargs)
 
 	elif function == 'softmax':
-		y_out = tf.nn.sigmoid(z)
-		expy = tf.exp(y_out)
-		sumexpy = tf.reduce_sum(expy)
-		output = tf.div(expy, sumexpy, **kwargs)
+		output = tf.nn.softmax(z, **kwargs)
 
 	elif function == 'elu':
 		output = tf.nn.elu(z, **kwargs)

tfomics/learn.py
@@ -4,7 +4,8 @@ import os, sys
 
 __all__ = [
 	"train_minibatch",
-	"train_variable_learning_rate"
+	"train_anneal_batch_size",
+	"train_anneal_learning_rate"
 ]
 
 
@@ -15,24 +16,24 @@ def train_minibatch(sess, nntrainer, data, batch_size=128, num_epochs=500,
 
 	# train model
 	for epoch in range(num_epochs):
-		if verbose == 1:
+		if verbose >= 1:
 			sys.stdout.write("\rEpoch %d out of %d \n"%(epoch+1, num_epochs))
+		else:
+			if epoch % 10 == 0:
+				sys.stdout.write("\rEpoch %d out of %d \n"%(epoch+1, num_epochs))
 
 		# training set
-		train_loss = nntrainer.train_epoch(sess, 
-											data['train'], 
+		train_loss = nntrainer.train_epoch(sess, data['train'], 
 											batch_size=batch_size, 
 											verbose=verbose, 
 											shuffle=shuffle)
-		nntrainer.add_loss(train_loss, 'train') 
-
 
 		# test current model with cross-validation data and store results
 		if 'valid' in data.keys():
-			valid_loss = nntrainer.test_model(sess, 
-												data['valid'], 
+			valid_loss = nntrainer.test_model(sess, data['valid'], 
 												name="valid", 
-												batch_size=batch_size)
+												batch_size=batch_size,
+												verbose=verbose)
 
 			# save model
 			nntrainer.save_model(sess, epoch)
@@ -47,9 +48,55 @@ def train_minibatch(sess, nntrainer, data, batch_size=128, num_epochs=500,
 
 
 
+def train_anneal_batch_size(sess, nntrainer, train, valid, batch_schedule, 
+							num_epochs=500, patience=10, verbose=1, shuffle=True):
+	"""Train a model with cross-validation data and test data
+			batch_schedule = {  0: 50, 
+								20: 100,
+								40: 500,
+								55: 1000,
+								50: 1500,
+								65: 2000
+								}	
+	"""
+	
+	# train model
+	for epoch in range(num_epochs):
+		if verbose == 1:
+			sys.stdout.write("\rEpoch %d out of %d \n"%(epoch+1, num_epochs))
+
+		# change learning rate if on schedule
+		if epoch in batch_schedule.keys():
+			batch_size = batch_schedule[epoch]
+
+		# training set
+		train_loss = nntrainer.train_epoch(sess, data['train'], 
+											batch_size=batch_size, 
+											verbose=verbose, 
+											shuffle=shuffle)
+
+		# test current model with cross-validation data and store results
+		if 'valid' in data.keys():
+			valid_loss = nntrainer.test_model(sess, data['valid'], 
+												name="valid", 
+												batch_size=batch_size,
+												verbose=verbose)
+
+			# save model
+			nntrainer.save_model(sess)
+
+			# check for early stopping
+			if patience:
+				status = nntrainer.early_stopping(valid_loss, epoch, patience)
+				if not status:
+					break
+
+	return nntrainer
 
 
-def train_variable_learning_rate(sess, nntrainer, train, valid, learning_rate_schedule, 
+
+
+def train_anneal_learning_rate(sess, nntrainer, train, valid, learning_rate_schedule, 
 						batch_size=128, num_epochs=500, patience=10, verbose=1, shuffle=True):
 	"""Train a model with cross-validation data and test data
 			learning_rate_schedule = {  0: 0.001
@@ -59,7 +106,6 @@ def train_variable_learning_rate(sess, nntrainer, train, valid, learning_rate_sc
 										}
 	"""
 
-
 	# train model
 	for epoch in range(num_epochs):
 		if verbose == 1:
@@ -67,8 +113,7 @@ def train_variable_learning_rate(sess, nntrainer, train, valid, learning_rate_sc
 
 		# change learning rate if on schedule
 		if epoch in learning_rate_schedule:
-			lr = np.float32(learning_rate_schedule[epoch])
-			nntrainer.set_learning_rate(lr)
+			nntrainer.placeholders['learning_rate'] = np.float32(learning_rate_schedule[epoch])
 
 		# training set
 		train_loss = nntrainer.train_epoch(sess, 
@@ -76,15 +121,14 @@ def train_variable_learning_rate(sess, nntrainer, train, valid, learning_rate_sc
 											batch_size=batch_size, 
 											verbose=verbose, 
 											shuffle=shuffle)
-		nntrainer.add_loss(train_loss, 'train') 
-
 
 		# test current model with cross-validation data and store results
 		if 'valid' in data.keys():
 			valid_loss = nntrainer.test_model(sess, 
 												data['valid'], 
 												name="valid", 
-												batch_size=batch_size)
+												batch_size=batch_size,
+												verbose=verbose)
 
 			# save model
 			nntrainer.save_model(sess)

tfomics/neuralnetwork.py
@@ -2,32 +2,29 @@ from __future__ import print_function
 import os, sys, time
 import tensorflow as tf
 import numpy as np
-from .optimize import *
-from .metrics import *
-from .utils import *
+from .optimize import build_loss, build_updates
+from .metrics import calculate_metrics
+#from .utils import *
 
 
 __all__ = [
 	"NeuralNet",
 	"NeuralTrainer",
-	"MonitorPerformance"
+	"MonitorPerformance",
+	"BatchGenerator"
 ]
-
 
 
 #------------------------------------------------------------------------------------------
 # Neural Network model class
 #------------------------------------------------------------------------------------------
 
-
-
 class NeuralNet:
 	"""Class to build a neural network and perform basic functions."""
 	
-	def __init__(self, network, input_vars):
+	def __init__(self, network, placeholders):
 		self.network = network
-		self.input_vars = input_vars
-		
+		self.placeholders = placeholders
 		self.saver = tf.train.Saver()
 
 
@@ -68,10 +65,9 @@ class NeuralNet:
 		return fmaps
 
 
-
 	def save_model_parameters(self, sess, filepath='model.ckpt'):
 		"""save model parameters to a file"""
-		print("saving model to: ", filepath)
+		print("  saving model to: ", filepath)
 		self.saver.save(sess, save_path=filepath)
 		
 
@@ -88,19 +84,26 @@ class NeuralNet:
 # Train neural networks class
 #----------------------------------------------------------------------------------------------------
 
-
 class NeuralTrainer():
+	""" class to train a neural network model """
 
-	def __init__(self, nnmodel, placeholders, optimization, save='best', filepath='.'):
+	def __init__(self, nnmodel, optimization=[], save='best', filepath='.'):
 		self.nnmodel = nnmodel
-		self.input_vars = nnmodel.input_vars
-		self.targets = placeholders['targets']
-		self.placeholders = placeholders
+		self.placeholders = nnmodel.placeholders
+		self.targets = self.placeholders['targets']
 		
+		# default optimizer if none given
+		if not optimization:
+			optimization = {}
+			optimization['objective'] = 'adam'
+			optimization['learning_rate'] = 0.001
+		else:
+			if 'objective' not in optimization.keys():
+				optimization['objective'] = 'adam'
+				optimization['learning_rate'] = 0.001
 		self.optimization = optimization    
 		self.objective = optimization['objective']
 		self.save = save
-
 		self.filepath = filepath
 		
 		# get predictions
@@ -114,7 +117,7 @@ class NeuralTrainer():
 		trainable_params = get_trainable(nnmodel.network)
 		self.train_step = self.updates.minimize(self.loss, var_list=trainable_params)
 
-
+		# instantiate monitor class to monitor performance
 		self.train_monitor = MonitorPerformance(name="train", objective=self.objective, verbose=1)
 		self.test_monitor = MonitorPerformance(name="test", objective=self.objective, verbose=1)
 		self.valid_monitor = MonitorPerformance(name="cross-validation", objective=self.objective, verbose=1)
@@ -127,6 +130,7 @@ class NeuralTrainer():
 		performance = MonitorPerformance('train', self.objective, verbose)
 		performance.set_start_time(start_time = time.time())
 
+		# instantiate batch generator
 		batch_generator = BatchGenerator(feed_X['inputs'], self.placeholders, batch_size, shuffle)
 		num_batches = batch_generator.get_num_batches()
 
@@ -137,34 +141,40 @@ class NeuralTrainer():
 			value += self.train_metric(results[2], feed_dict[self.targets])
 			performance.add_loss(results[1])
 			performance.progress_bar(i+1., num_batches, value/(i+1))
-		if verbose:
-			print("")
-		return performance.get_mean_loss()
+		if verbose > 1:
+			print(" ")
+
+		# calculate mean loss and store
+		loss = performance.get_mean_loss()
+		self.add_loss(loss, 'train')
+
+		return loss
 
 
 	def train_metric(self, predictions, y):
 		"""metric to monitor performance during training"""
 
 		if self.objective == 'categorical':
-			return np.mean(np.argmax(predictions, axis=1) == np.argmax(y, axis=1))
-		
+			if y.shape[1] > 1:
+				return np.mean(np.argmax(predictions, axis=1) == np.argmax(y, axis=1))
+			else:
+				return np.mean(np.argmax(predictions, axis=1) == y)
+				
 		elif self.objective == 'binary':
 			return np.mean(np.round(predictions) == y)
 		
 		elif self.objective == 'squared_error':
-			
 			num_dims = y.shape[1]
-			
 			C = 0
 			for i in range(num_dims):
 				C += np.corrcoef(predictions[:,i],y[:,i])[0][1]
 			return C/num_dims
 
 		
-		
 	def test_model(self, sess, feed_X, batch_size=128, name='test', verbose=1):
 		"""perform a complete forward pass, store and print(results)"""
 
+		# instantiate monitor performance and batch generator
 		performance = MonitorPerformance('test',self.objective, verbose)
 		batch_generator = BatchGenerator(feed_X['inputs'], self.placeholders, batch_size, shuffle=False)
 
@@ -180,15 +190,17 @@ class NeuralTrainer():
 		label = np.vstack(label)
 		test_loss = performance.get_mean_loss()
 
-		if verbose:
-			if name == "train":
-				self.train_monitor.update(test_loss, prediction, label)
+		if name == "train":
+			self.train_monitor.update(test_loss, prediction, label)
+			if verbose >= 1:
 				self.train_monitor.print_results(name)
-			elif name == "valid":
-				self.valid_monitor.update(test_loss, prediction, label)
+		elif name == "valid":
+			self.valid_monitor.update(test_loss, prediction, label)
+			if verbose >= 1:
 				self.valid_monitor.print_results(name)
-			elif name == "test":
-				self.test_monitor.update(test_loss, prediction, label)
+		elif name == "test":
+			self.test_monitor.update(test_loss, prediction, label)
+			if verbose >= 1:
 				self.test_monitor.print_results(name)
 		return test_loss
 
@@ -210,7 +222,7 @@ class NeuralTrainer():
 		if self.save == 'best':
 			min_loss, min_epoch = self.valid_monitor.get_min_loss()
 			if self.valid_monitor.loss[-1] <= min_loss:
-				print('lower cross-validation found')
+				print('  lower cross-validation found')
 				filepath = self.filepath + '_best.ckpt'
 				self.nnmodel.save_model_parameters(sess, filepath)
 		elif self.save == 'all':
@@ -310,7 +322,7 @@ class MonitorPerformance():
 
 
 	def print_results(self, name):
-		if self.verbose == 1:
+		if self.verbose >= 1:
 			if name == 'test':
 				name += ' '
 
@@ -326,9 +338,8 @@ class MonitorPerformance():
 				print("  " + name + " rsquare:\t{:.5f}+/-{:.5f}".format(mean_vals[1], error_vals[1]))
 				print("  " + name + " slope:\t\t{:.5f}+/-{:.5f}".format(mean_vals[2], error_vals[2]))
 
-
 	def progress_bar(self, epoch, num_batches, value, bar_length=30):
-		if self.verbose == 1:
+		if self.verbose > 1:
 			remaining_time = (time.time()-self.start_time)*(num_batches-epoch)/epoch
 			percent = epoch/num_batches
 			progress = '='*int(round(percent*bar_length))
@@ -344,7 +355,7 @@ class MonitorPerformance():
 
 	def save_metrics(self, filepath):
 		savepath = filepath + "_" + self.name +"_performance.pickle"
-		print("saving metrics to " + savepath)
+		print("  saving metrics to " + savepath)
 
 		f = open(savepath, 'wb')
 		cPickle.dump(self.name, f, protocol=cPickle.HIGHEST_PROTOCOL)
@@ -354,12 +365,14 @@ class MonitorPerformance():
 		f.close()
 
 
+
 #--------------------------------------------------------------------------------------------------
 # Batch Generator Class
 #--------------------------------------------------------------------------------------------------
 
-
 class BatchGenerator():
+	""" helper class to generate mini-batches """
+
 	def __init__(self, X, placeholders, batch_size=128, shuffle=False):
 
 		if isinstance(X, list):

tfomics/optimize.py
@@ -177,13 +177,13 @@ def cost_function(predictions, targets, objective, **kwargs):
 			predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
 		
 	if objective == 'binary':   
+		predictions = tf.nn.sigmoid(predictions)
 		loss = -tf.reduce_mean(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions))
 		#loss = tf.nn.sigmoid_cross_entropy_with_logits(predictions, targets, name='binary_loss')
 
 	elif objective == 'categorical':
-		predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
-		#loss = -tf.reduce_sum(targets*tf.log(predictions))
-		loss = tf.nn.softmax_cross_entropy_with_logits(predictions, targets, name='softmax_loss')
+		loss = -tf.reduce_mean(tf.reduce_sum(targets*tf.log(predictions), axis=1))
+		#loss = tf.nn.softmax_cross_entropy_with_logits(predictions, targets, name='softmax_loss')
 	
 	elif objective == 'squared_error':    
 		loss = tf.reduce_mean(tf.square(predictions - targets))