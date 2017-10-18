c9405f104fffa44035680f1d7412852977ab0402
p-koo <peter_koo@harvard.edu>
2017-10-17 23:27:48 -0400

    updates

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from __future__ import print_function 
+from __future__ import print_function
 import os, sys
 import numpy as np
 import tensorflow as tf
@@ -21,31 +21,33 @@ def initialize_session(placeholders=None):
 
 	# initialize variables
 	if placeholders is None:
-		sess.run(tf.global_variables_initializer()) 
+		sess.run(tf.global_variables_initializer())
 
 	else:
+
+		# below fixes a bug in older versions of tensorflow (not needed in version r1.0)
 		if 'is_training' in placeholders:
-			sess.run(tf.global_variables_initializer(), feed_dict={placeholders['is_training']: True}) 
+			sess.run(tf.global_variables_initializer(), feed_dict={placeholders['is_training']: True})
 		else:
-			sess.run(tf.global_variables_initializer()) 
+			sess.run(tf.global_variables_initializer())
 	return sess
 
 
 def placeholder(shape, dtype=tf.float32, name=None):
 	return tf.placeholder(dtype=dtype, shape=shape, name=name)
-	
-			
+
+
 class Variable():
 	def __init__(self, var, shape, **kwargs):
 
 		self.l1_regularize = True
 		if 'l1' in kwargs.keys():
 			self.l1_regularize = kwargs['l1']
-		
+
 		self.l2_regularize = True
 		if 'l2' in kwargs.keys():
 			self.l2_regularize = kwargs['l2']
-		
+
 		if 'regularize' in kwargs.keys():
 			if not kwargs['regularize']:
 				self.l1_regularize = False
@@ -58,33 +60,33 @@ class Variable():
 		self.name = None
 		if 'name' in kwargs.keys():
 			self.name = kwargs['name']
-			
+
 		self.shape = shape
 		variable = var(shape)
 		self.variable = tf.Variable(variable)
 
-		
+
 	def get_variable(self):
 		return self.variable
-	
+
 	def get_shape(self):
 		return self.shape
-	
+
 	def set_l1_regularize(self, status):
 		self.l1_regularize = status
-		
+
 	def set_l2_regularize(self, status):
 		self.l2_regularize = status
-		
+
 	def set_trainable(self, status):
 		self.trainable = status
-			
+
 	def is_l1_regularize(self):
 		return self.l1_regularize
-		
+
 	def is_l2_regularize(self):
 		return self.l2_regularize
-		
+
 	def is_trainable(self):
 		return self.trainable
 
@@ -115,7 +117,7 @@ def normalize_pwm(pwm, factor=None):
 
 def meme_generate(W, output_file='meme.txt', prefix='filter', factor=None):
 
-	# background frequency        
+	# background frequency
 	nt_freqs = [1./4 for i in range(4)]
 
 	# open file for writing
@@ -143,4 +145,3 @@ def meme_generate(W, output_file='meme.txt', prefix='filter', factor=None):
 		f.write('\n')
 
 	f.close()
-	
