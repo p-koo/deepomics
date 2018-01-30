f547d60d0a9a8f881948cc898491e2e018d07cdd
p-koo <peter_koo@harvard.edu>
2018-01-30 11:46:34 -0500

    fixed reduce axis

--- objectives.py
+++ objectives.py
@@ -4,18 +4,21 @@ import numpy as np
 
 
 def binary_cross_entropy(targets, predictions):
+
 	predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
-	return tf.reduce_sum(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions), axis=1)
+	return tf.reduce_sum(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions), axis=get_reduce_axis(targets))
 
 
 def categorical_cross_entropy(targets, predictions):
+
 	predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
 		#loss = -tf.reduce_mean(tf.reduce_sum(targets*tf.log(predictions), axis=1))
-	return tf.reduce_sum(targets*tf.log(predictions), axis=1)
+	return tf.reduce_sum(targets*tf.log(predictions), axis=get_reduce_axis(targets))
 
 
 def squared_error(targets, predictions):
-	return tf.reduce_sum(tf.square(targets - predictions), axis=1)
+
+	return tf.reduce_sum(tf.square(targets - predictions), axis=get_reduce_axis(targets))
 
 
 def categorical_cross_entropy2D(targets, predictions, shape):
@@ -47,7 +50,7 @@ def elbo_gaussian_gaussian(targets, X_mu, X_logvar, Z_mu, Z_logvar, KL_weight=No
 
 	#log_likelihood = -tf.reduce_sum(tf.square(targets-X_mu), axis=1)
 	const = tf.constant(-0.5*np.log(2*np.float32(np.pi)), dtype=tf.float32)
-	log_likelihood = tf.reduce_sum(const - 0.5*X_logvar - 0.5*tf.divide(tf.square(targets-X_mu),tf.exp(X_logvar)), axis=1)
+	log_likelihood = tf.reduce_sum(const - 0.5*X_logvar - 0.5*tf.divide(tf.square(targets-X_mu),tf.exp(X_logvar)), axis=get_reduce_axis(targets))
 
 	if KL_weight is None:
 		KL_weight = 1.0
@@ -63,7 +66,7 @@ def elbo_gaussian_binary(targets, X_mu, Z_mu, Z_logvar, KL_weight=None):
 
 	# calculate reconstructed likelihood
 	X_mu = tf.clip_by_value(X_mu, 1e-7, 1-1e-7)
-	log_likelihood = tf.reduce_sum(targets*tf.log(X_mu) + (1.0-targets)*tf.log(1.0-X_mu), axis=1)
+	log_likelihood = tf.reduce_sum(targets*tf.log(X_mu) + (1.0-targets)*tf.log(1.0-X_mu), axis=get_reduce_axis(targets))
 
 	if KL_weight is None:
 		KL_weight = 1.0
@@ -106,7 +109,7 @@ def elbo_softmax_normal(targets, X, Z, Z_shape, KL_weight=None):
 	kl_divergence = tf.reduce_sum(kl_tmp, [1,2])
 
 	# calculate reconstructed likelihood
-	log_likelihood = -tf.reduce_sum(tf.square(targets-X), axis=1)
+	log_likelihood = -tf.reduce_sum(tf.square(targets-X), axis=get_reduce_axis(targets))
 	#log_likelihood = tf.reduce_sum(const - 0.5*X_logvar - 0.5*tf.divide(tf.square(targets-X_mu),tf.exp(X_logvar)), axis=1)
 
 	if KL_weight is None:
@@ -127,7 +130,7 @@ def elbo_softmax_binary(targets, X, Z, Z_shape, KL_weight=None):
 
 	# calculate reconstructed likelihood
 	X = tf.clip_by_value(X, 1e-7, 1-1e-7)
-	log_likelihood = tf.reduce_sum(targets*tf.log(X) + (1.0-targets)*tf.log(1.0-X), axis=1)
+	log_likelihood = tf.reduce_sum(targets*tf.log(X) + (1.0-targets)*tf.log(1.0-X), axis=get_reduce_axis(targets))
 
 	if KL_weight is None:
 		KL_weight = 1.0
@@ -160,3 +163,16 @@ def elbo_softmax_softmax(targets, X, Z, X_shape, Z_shape, KL_weight=None):
 		KL_weight = 1.0
 
 	return log_likelihood + KL_weight*kl_divergence
+
+
+
+def get_reduce_axis(targets):
+
+	dims = len(targets.get_shape())
+	if dims == 2:
+		axis = 1
+	elif dims == 4:
+		axis = [1,2,3]
+
+	return axis
+
