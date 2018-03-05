fixed vae softmax

layers/noise.py
@@ -92,17 +92,27 @@ def gumbel_softmax_sample(logits, temperature):
 class CategoricalSampleLayer(BaseLayer):
 	def __init__(self, incoming, temperature, hard=False, **kwargs):
 
+
+		shape = incoming.get_output_shape().as_list()
+		num_classes = shape[2]
+		seq_length = shape[1]
+
 		self.incoming_shape = incoming.get_output_shape()
 		self.output_shape = self.incoming_shape
 		self.temperature = temperature
 		self.hard = hard
 
-		self.output = gumbel_softmax_sample(incoming.get_output(), temperature)
+		incoming_reshape = tf.reshape(incoming.get_output(), [-1, num_classes])
+		self.output = gumbel_softmax_sample(incoming_reshape, temperature)
+
+
 		if self.hard:
 			k = tf.shape(self.output)[-1]
 			#y_hard = tf.cast(tf.one_hot(tf.argmax(y,1),k), y.dtype)
 			y_hard = tf.cast(tf.equal(self.output, tf.reduce_max(self.output,1,keep_dims=True)), self.output.dtype)
 			self.output = tf.stop_gradient(y_hard - self.output) + self.output
+
+		self.output = tf.reshape(self.output, [-1, seq_length, num_classes])
 
 	def get_input_shape(self):
 		return self.incoming_shape

objectives.py
@@ -6,8 +6,13 @@ import numpy as np
 def binary_cross_entropy(targets, predictions):
 
 	predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
-	return tf.reduce_sum(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions), axis=get_reduce_axis(targets))
+	return tf.reduce_mean(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions), axis=get_reduce_axis(targets))
 
+
+def weighted_binary_cross_entropy(targets, predictions, class_weights):
+
+	predictions = tf.clip_by_value(predictions,1e-7,1-1e-7)
+	return tf.reduce_mean(class_weights*(targets*tf.log(predictions) + (1-targets)*tf.log(1-predictions)), axis=get_reduce_axis(targets))
 
 def categorical_cross_entropy(targets, predictions):
 
@@ -175,4 +180,3 @@ def get_reduce_axis(targets):
 		axis = [1,2,3]
 
 	return axis
-

utils.py
@@ -105,10 +105,11 @@ def make_directory(path, foldername, verbose=1):
 	return outdir
 
 
-def normalize_pwm(pwm, factor=None):
+def normalize_pwm(pwm, factor=None, max=None):
 
-	MAX = np.max(np.abs(pwm))
-	pwm = pwm/MAX
+	if not max:
+		max = np.max(np.abs(pwm))
+	pwm = pwm/max
 	if factor:
 		pwm = np.exp(pwm*factor)
 	norm = np.outer(np.ones(pwm.shape[0]), np.sum(np.abs(pwm), axis=0))

visualize.py
@@ -163,13 +163,18 @@ def plot_filter_logos(W, figsize=(10,7), height=25, nt_width=10, norm=0, alphabe
 	grid = mpl.gridspec.GridSpec(num_rows, num_cols)
 	grid.update(wspace=0.2, hspace=0.2, left=0.1, right=0.2, bottom=0.1, top=0.2)
 	fig = plt.figure(figsize=figsize);
+	if norm:
+		MAX = np.max(W)
+	else:
+		MAX = None
+
 	for i in range(num_filters):
 		plt.subplot(grid[i]);
 		if norm_factor:
-			W_norm = normalize_pwm(W[i], factor=norm_factor)
+			W_norm = normalize_pwm(W[i], factor=norm_factor, max=MAX)
 		else:
 			W_norm = W[i]
-		logo = seq_logo(W_norm, height=height, nt_width=nt_width, norm=norm, alphabet=alphabet)
+		logo = seq_logo(W_norm, height=height, nt_width=nt_width, norm=0, alphabet=alphabet)
 		plot_seq_logo(logo, nt_width=nt_width, step_multiple=None)
 		#if np.mod(i, num_rows) != 0:
 		plt.yticks([])